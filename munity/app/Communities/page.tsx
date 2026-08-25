154936d71fae2f32eb5e466ad242f5cdc623696f
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2026-08-25 20:43:42 +0000

    12: Community page and connected pages supabase logic

--- munity/app/Communities/page.tsx
+++ munity/app/Communities/page.tsx
@@ -1,7 +1,18 @@
 import { CommunitiesView } from "@/components/communities/CommunitiesView";
+import { getCommunities, getMyMembershipIds } from "@/lib/communities/queries";
 import { getMemberLoggedIn } from "@/lib/member-auth";
 
 export default async function CommunitiesPage() {
   const isLoggedIn = await getMemberLoggedIn();
-  return <CommunitiesView isLoggedIn={isLoggedIn} />;
+  const [communities, membershipIds] = await Promise.all([
+    getCommunities(),
+    getMyMembershipIds(),
+  ]);
+  return (
+    <CommunitiesView
+      isLoggedIn={isLoggedIn}
+      communities={communities}
+      initialMembershipIds={membershipIds}
+    />
+  );
 }
